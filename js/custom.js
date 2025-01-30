3ed7f6bedfcffe2a65fa40d38886727a419e15df
DESKTOP-G2SHP0A\Iulian <iulianbaiburi99@yahoo.com>
2025-01-30 18:35:11 +0200

    - update pentru slider

--- js/custom.js
+++ js/custom.js
@@ -71,28 +71,32 @@ const app = Vue.createApp({
 /*START ABOUT PAGE STYLE*/
 
 (function(window, document, $, undefined) {
-	var $slides, $btnArr;
+  var $slides, $btnArr;
 
-	function onClick(e) {
-		var $target = $(e.target);
-		if ($target.hasClass('slide') && !$target.hasClass('active') && !$target.siblings().hasClass('active')) {
-			$target.removeClass('anim-in last-viewed').addClass('active')
-			$target.siblings().removeClass('anim-in last-viewed').addClass('anim-out');
-		}
-	}
+  function onClick(e) {
+      var $target = $(e.target);
+      if ($target.hasClass('slide') && !$target.hasClass('active') && !$target.siblings().hasClass('active')) {
+          $target.removeClass('anim-in last-viewed').addClass('active');
+          $target.siblings().removeClass('anim-in last-viewed').addClass('anim-out');
 
-	function closeSlide(e) {
-		var $slide = $(e.target).parent();
-		$slide.removeClass('active anim-in').addClass('last-viewed');
-		$slide.siblings().removeClass('anim-out').addClass('anim-in');
-	}
+          $('html, body').css('width', '100vw');
+      }
+  }
 
-	$(function() {
-		$slides = $('.slide');
-		$btnArr = $slides.find('.btn-close');
-		$slides.on('click', onClick);
-		$btnArr.on('click', closeSlide);
-	});
+  function closeSlide(e) {
+      var $slide = $(e.target).parent();
+      $slide.removeClass('active anim-in').addClass('last-viewed');
+      $slide.siblings().removeClass('anim-out').addClass('anim-in');
+
+      $('html, body').css('width', '200vw');
+  }
+
+  $(function() {
+      $slides = $('.slide');
+      $btnArr = $slides.find('.btn-close');
+      $slides.on('click', onClick);
+      $btnArr.on('click', closeSlide);
+  });
 })(this, document, jQuery);
 
 /*END ABOUT PAGE STYLE*/
